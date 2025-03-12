f18b52178e2f4322a2cc32b6523af88a5e729890
davydkinia <davydkinia@gmail.com>
2025-03-12 19:01:32 +0300

    Add your commit

---
+++ deploy/laravel/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "=== Copy .env ==="
+cp -v /.env /application/.env
+
+echo "=== Switching shell to /application folder ==="
+cd /application
+
+echo "=== Run composer install ==="
+composer install
+
+echo "=== Run npm install ==="
+npm install
+
+echo "=== Run npm run build ==="
+npm run build
+
+echo "=== Run php artisan key:generate ==="
+php artisan key:generate
+
+echo "=== Run php artisan storage:link ==="
+php artisan storage:link
+
+echo "=== Run php artisan migrate ==="
+php artisan migrate
+
+echo "=== Run /usr/sbin/php-fpm8.4 -O ==="
+/usr/sbin/php-fpm8.4 -O
